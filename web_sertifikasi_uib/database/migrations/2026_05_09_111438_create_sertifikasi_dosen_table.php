12bb948392ec2b5a18c55e0cf01976b7ac848945
RemanssonLim <gitreman@gmail.com>
2026-05-09 20:36:34 +0700

    update-

--- web_sertifikasi_uib/database/migrations/2026_05_09_111438_create_sertifikasi_dosen_table.php
+++ web_sertifikasi_uib/database/migrations/2026_05_09_111438_create_sertifikasi_dosen_table.php
@@ -26,6 +26,14 @@ return new class extends Migration
 
     $table->enum('status', ['terdaftar', 'lulus', 'tidak_lulus'])
           ->default('terdaftar');
+    
+    $table->enum('status_pembayaran', [
+    'belum lunas',
+    'lunas',
+    'dispensasi'
+    ])->default('belum lunas');
+
+    $table->text('keterangan')->nullable();
 
     $table->timestamps();
 
--- web_sertifikasi_uib/database/migrations/2026_05_09_113232_create_sertifikasi_user_umum_table.php
+++ web_sertifikasi_uib/database/migrations/2026_05_09_113232_create_sertifikasi_user_umum_table.php
@@ -24,6 +24,14 @@ return new class extends Migration
     $table->enum('status', ['terdaftar', 'lulus', 'tidak_lulus'])
           ->default('terdaftar');
 
+    $table->enum('status_pembayaran', [
+        'belum lunas',
+        'lunas',
+        'dispensasi'
+    ])->default('belum lunas');
+
+    $table->text('keterangan')->nullable();
+
     $table->timestamps();
 
     $table->primary(['user_umum_id', 'sertifikasi_id']);
