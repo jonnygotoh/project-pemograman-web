367ca7a684e0283d2eb89fffa5797a661a340a01
jonnyanderson <jonnyanderso089@gmail.com>
2026-05-10 12:55:56 +0700

    Update web.php

--- web_sertifikasi_uib/routes/web.php
+++ web_sertifikasi_uib/routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/Mahasiswa', [MahasiswaController::class, 'mahasiswa'])
+    ->name('mahasiswa');
+
+Route::get('/Dosen', [MahasiswaController::class, 'dosen'])
+    ->name('dosen');
+
+Route::get('/Admin', [MahasiswaController::class, 'admin'])
+    ->name('admin');
+
+Route::get('/Sertifikasi', [MahasiswaController::class, 'sertifikasi'])
+    ->name('sertifikasi');
+
+Route::get('/Seminar', [MahasiswaController::class, 'seminar'])
+    ->name('seminar');
