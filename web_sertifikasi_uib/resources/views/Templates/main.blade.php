ae850d55e9ade6718e1d3a1d792a4e3b95d2a988
jonnyanderson <jonnyanderso089@gmail.com>
2026-05-19 19:24:08 +0700

    p

--- web_sertifikasi_uib/resources/views/Templates/main.blade.php
+++ web_sertifikasi_uib/resources/views/Templates/main.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-      
+
+      <script src="https://unpkg.com/lucide@latest"></script>
       <link rel="stylesheet" href="{{ asset('css/uib.css') }}">
 </head>
 <body>
