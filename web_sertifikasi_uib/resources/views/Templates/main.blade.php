56cfd5e4a19b8cfc33f3d69310efda67a665bc13
RemanssonLim <gitreman@gmail.com>
2026-05-31 00:15:54 +0700

    css read improve

--- web_sertifikasi_uib/resources/views/Templates/main.blade.php
+++ web_sertifikasi_uib/resources/views/Templates/main.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/uib.css') }}">
-    <script src="https://unpkg.com/lucide@latest"></script>
+<link rel="stylesheet" href="{{ asset('css/uib.css') }}?v={{ filemtime(public_path('css/uib.css')) }}">
+<script src="https://unpkg.com/lucide@latest"></script>
 </head>
 <body>
 
