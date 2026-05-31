44d0eb2a5246c6c4a879de61f002b08c27b820d4
RemanssonLim <gitreman@gmail.com>
2026-05-31 13:51:02 +0700

    Hapus auto buat database-

--- web_sertifikasi_uib/app/Providers/AppServiceProvider.php
+++ web_sertifikasi_uib/app/Providers/AppServiceProvider.php
@@ -19,49 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-        public function boot(): void
+    public function boot(): void
     {
-        $dbName = 'sertifikasi_uib';
-
-        /*
-        |--------------------------------------------------------------------------
-        | AUTO UPDATE .ENV
-        |--------------------------------------------------------------------------
-        */
-
-        $envPath = base_path('.env');
-
-        if (file_exists($envPath)) {
-
-            $env = file_get_contents($envPath);
-            $env = preg_replace(
-                '/DB_DATABASE=.*/',
-                'DB_DATABASE=' . $dbName,
-                $env
-            );
-
-            file_put_contents($envPath, $env);
-        }
-
-        /*
-        |--------------------------------------------------------------------------
-        | CREATE DATABASE IF NOT EXISTS
-        |--------------------------------------------------------------------------
-        */
-
-        try {
-
-            Config::set('database.connections.mysql.database', null);
-            DB::purge('mysql');
-            DB::statement("CREATE DATABASE IF NOT EXISTS `$dbName`");
-            Config::set('database.connections.mysql.database', $dbName);
-            DB::reconnect('mysql');
-
-        } catch (\Exception $e) {
-
-            // optional debug
-            // dd($e->getMessage());
-
-        }
+        //
     }
 }
